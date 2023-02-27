978d44f2f051e7abe040989eccac8604d81f3eca
Dmitry <dsilichev@gmail.com>
2023-02-27 17:30:20 +0300

    using_classnames_for_styling

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ function App() {
   return (
     <div>
       <div>
-        <Button success primary rounded>Click me!</Button>
+        <Button secondary outline rounded>Click me!</Button>
       </div>
       <div>
-        <Button>Seal!</Button>
+        <Button danger outline>Seal!</Button>
       </div>
       <div>
-        <Button>Deal!</Button>
+        <Button warning>Deal!</Button>
       </div>
       <div>
-        <Button>Feel!</Button>
+        <Button secondary outline>Feel!</Button>
+      </div>
+      <div>
+        <Button primary rounded>Steal!</Button>
       </div>
     </div>
   );
